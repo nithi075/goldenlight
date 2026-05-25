2d08e6dccbbf5989b34cd844b557b8d43d8dde48
Sam Nithish <nithishraj8055@gmail.com>
2026-05-25 13:21:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import FilmSection from "./components/FilmSection/FlimSection";
 import About from "./components/About/About";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Gallery from "./components/Gallery/Gallery";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/footer/footer";
 import Navbar from "./components/Navbar/Navbar";
 import Storytelling from "./components/StoryTelling/StoryTelling";
 import WeddingStory from "./components/WeddingStory/WeddingStory";
